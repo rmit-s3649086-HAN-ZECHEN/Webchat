72ae8026e8b7d21e87f5738e08b39ecb7eaa2fd2
Han <han13066022700@gmail.com>
2020-05-21 12:21:20 +1000

    first version debug2

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send("Welcome to the home page!");
+app.get('/', (req, res) => {
+   res.send({'hi': 'there'})
 });
 
-const server = app.listen(process.env.PORT || 5000, () => {
-    const host = server.address().address;
-    const port = server.address().port;
 
-    console.log('Example app listening at http://${host}:${port}');
-});
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
